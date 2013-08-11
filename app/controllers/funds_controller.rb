5d3909f347addf79337b6a7919eafe2045d638cb
Andrés Mejía <andmej@gmail.com>
2013-08-11 19:11:57 -0300

    Implement Funds#show action.

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -2,4 +2,8 @@ class FundsController < ApplicationController
   def index
     @funds = Fund.order(corporate_name: :asc)
   end
+
+  def show
+    @fund = Fund.find_by_ticker(params[:id]) || Fund.find(params[:id])
+  end
 end
